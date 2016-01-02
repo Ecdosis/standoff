ee030bffa7d0f7f3cd8e67dd39b9aefa22ac1b10
desmond <desmond.allan.schmidt@gmail.com>
2016-01-03 03:23:15 +1000

    Fixed rebuild.sh

--- stripper/rebuild.sh
+++ stripper/rebuild.sh
@@ -1,5 +1,5 @@
 if [ $USER = "root" ]; then
-  gcc -DCOMMANDLINE -DHAVE_EXPAT_CONFIG_H -DHAVE_MEMMOVE -Iinclude -O0 -g3 -Wall src/*.c -lm -lexpat -laspell -ltidy -licuuc -o stripper
+  gcc -DCOMMANDLINE -DHAVE_EXPAT_CONFIG_H -DHAVE_MEMMOVE -Iinclude -O0 -g3 -Wall src/*.c -lm -lexpat -laspell -ltidy -licuuc -licuio -o stripper
   cp stripper /usr/local/bin/
 else
 	echo "Need to be root. Did you use sudo?"
